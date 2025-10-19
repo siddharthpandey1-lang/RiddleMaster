e0441ee92af5fcf1b3e352e0fb2077b5f476c283
Siddhart <Siddharth.pandey1@gmail.com>
2025-10-19 19:44:31 +0530

    Riddle3.py

---
+++ Riddle3.py
@@ -0,0 +1,6 @@
+print("A thing which has hands but cannot clap. What am I?")
+answer = input("Your answer: ")
+if answer.lower() == "clock":
+    print("Correct! Well done.")
+else:
+    print("Incorrect, try again.")
