ae78aed49affdb2aadb92450d501cc3f9477119c
Siddhart <Siddharth.pandey1@gmail.com>
2025-10-19 19:43:25 +0530

    Riddle2

---
+++ riddle2.py
@@ -0,0 +1,6 @@
+print("im a thing which has keys but cant open locks. what am i?")
+answer = input("Your answer: ")
+if answer.lower() == "piano":
+    print("Correct! Well done.")
+else:
+    print("Incorrect, try again.")
