ecfec4cd8c765e1f0831537e6782fa00f9b41d35
Siddhart <Siddharth.pandey1@gmail.com>
2025-10-19 19:42:21 +0530

    Riddle1

---
+++ Riddle1.py
@@ -0,0 +1,6 @@
+print("Im a thing which is in kitchens and also in bathrooms. What am I?")
+answer = input("Your answer: ")
+if answer.lower() == "sink":
+    print("Correct! Well done.")
+else:
+    print("Incorrect try again.")
