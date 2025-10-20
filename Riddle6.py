a9904678d6752945c4f841c3cf21bf2d548559e1
Siddhart <Siddharth.pandey1@gmail.com>
2025-10-20 07:01:12 +0530

    riddle 6

---
+++ Riddle6.py
@@ -0,0 +1,6 @@
+print("You see me once in June, twice in November, but not at all in May. What am I?")
+answer = input()
+if answer == "e":
+    print("Correct! You've solved Riddle 6.")
+else:
+    print("Incorrect. The answer is the letter 'e'.")
