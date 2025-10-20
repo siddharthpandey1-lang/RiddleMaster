5f08ad8aa1666548296c61ec76a8e898d75a0798
Siddhart <Siddharth.pandey1@gmail.com>
2025-10-20 07:02:41 +0530

    Riddle7

---
+++ riddle7.py
@@ -0,0 +1,6 @@
+print("The more you take, the more you leave behind. What am I?")
+answer = input()
+if answer.lower() == "footsteps":
+    print("Correct! You've solved Riddle 7.")
+else:
+    print("Incorrect. The answer is 'footsteps'.")
