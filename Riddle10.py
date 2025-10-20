39b0641acd0da0a953d63af260b28ef3da1393aa
Siddhart <Siddharth.pandey1@gmail.com>
2025-10-20 07:07:30 +0530

    Riddle10

---
+++ Riddle10.py
@@ -0,0 +1,9 @@
+print("The more you pack me, the lighter I become. What am I?")
+answer = input()
+if answer.lower() == "hole":
+    print("Correct! You've solved Riddle 10.")
+else:
+    print("Incorrect. The answer is 'hole'.")
+print("congratulations! You've completed all the riddles.")
+print("Your reward is the satisfaction of a job well done and the knowledge that you are a true Riddle Master!")
+print("Thank you for playing!")
