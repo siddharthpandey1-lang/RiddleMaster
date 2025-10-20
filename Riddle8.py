1891be7704707961eb7bca638918986f2199396c
Siddhart <Siddharth.pandey1@gmail.com>
2025-10-20 07:04:21 +0530

    Riddle8

---
+++ Riddle8.py
@@ -0,0 +1,6 @@
+print("I speak without a mouth and hear without ears. I have no body, but I come alive with wind. What am I?")
+answer = input()
+if answer.lower() == "echo":
+    print("Correct! You've solved Riddle 8.")
+else:
+    print("Incorrect. The answer is 'echo'.")
