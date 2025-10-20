d188a054329c530d1007d8f86b450c3aded48336
Siddhart <Siddharth.pandey1@gmail.com>
2025-10-20 07:05:11 +0530

    Riddle9

---
+++ Riddle9.py
@@ -0,0 +1,6 @@
+print("You look at me, I look back. You speak, I mimic. But I never blink. What am I?")
+answer = input()
+if answer.lower() == "mirror":
+    print("Correct! You've solved Riddle 9.")
+else:
+    print("Incorrect. The answer is 'mirror'.")
